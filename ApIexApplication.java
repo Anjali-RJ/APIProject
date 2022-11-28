7d79ebf5158a1c9780f164027672ce7f834c524b
Anjali-RJ <115079041+Anjali-RJ@users.noreply.github.com>
2022-11-28 14:28:02 +0530

    Add files via upload

---
+++ ApIexApplication.java
@@ -0,0 +1,13 @@
+package com.kiranacademy.APIex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIexApplication.class, args);
+	}
+
+}
